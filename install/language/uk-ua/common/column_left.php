5bfd29f3d50f4b6f0b466173d2e110ff2e49bd38
KravchenkoPaul <poul.kravchenko@gmail.com>
2023-03-27 03:03:19 +0300

    Add opencart.sql. Set Ukrainian lang by default

---
+++ install/language/uk-ua/common/column_left.php
@@ -0,0 +1,8 @@
+<?php
+// Text
+$_['text_license']       = 'Ліцензійна угода';
+$_['text_installation']  = 'Pre-Installation';
+$_['text_configuration'] = 'Configuration';
+$_['text_upgrade']       = 'Оновлення';
+$_['text_finished']      = 'Finished';
+$_['text_language']      = 'Мова';
