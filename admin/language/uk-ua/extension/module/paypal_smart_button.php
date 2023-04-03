cd68d7c739ac56bde326db68cb1d3a730ca64243
KravchenkoPaul <poul.kravchenko@gmail.com>
2023-04-03 18:04:10 +0300

    Adding PayPal Smart Button lang files

---
+++ admin/language/uk-ua/extension/module/paypal_smart_button.php
@@ -0,0 +1,67 @@
+<?php
+// Heading
+$_['heading_title']    					= 'PayPal Smart Button';
+
+// Text
+$_['text_extensions']   				= 'Доповнення';
+$_['text_edit']        					= 'Налаштування модуля PayPal';
+$_['text_general']				 	 	= 'Основні';
+$_['text_product_page']					= 'Сторінка товару';
+$_['text_cart_page']					= 'Сторінка кошика';
+$_['text_insert_prepend']				= 'Додати на початку';
+$_['text_insert_append']				= 'Додати у кінці';
+$_['text_insert_before']				= 'Додати перед';
+$_['text_insert_after']					= 'Додати після';
+$_['text_align_left']					= 'Ліворуч';
+$_['text_align_center']					= 'По центру';
+$_['text_align_right']					= 'Праворуч';
+$_['text_small']			 			= 'Маленька';
+$_['text_medium']			 	 		= 'Середня';
+$_['text_large']			 	 		= 'Велика';
+$_['text_responsive']			 	 	= 'Адаптивна';
+$_['text_gold']			 	 			= 'Золотий';
+$_['text_blue']			 	 			= 'Синій';
+$_['text_silver']			 	 		= 'Срібний';
+$_['text_white']			 	 		= 'Білий';
+$_['text_black']			 	 		= 'Чорний';
+$_['text_pill']			 	 			= 'Таблетка';
+$_['text_rect']			 	 			= 'Прямокутник';
+$_['text_checkout']			 	 		= 'Оформити';
+$_['text_pay']			 	 			= 'Оплата';
+$_['text_buy_now']			 	 		= 'Сплатити зараз';
+$_['text_pay_pal']			 	 		= 'PayPal';
+$_['text_installment']			 	 	= 'Часткова оплата';
+$_['text_text']							= 'Текстове повідомлення';
+$_['text_flex']							= 'Банер Flex';
+$_['text_yes']			 	 			= 'Так';
+$_['text_no']			 	 			= 'Ні';
+
+// Entry
+$_['entry_status']     					= 'Статус';
+$_['entry_product_page_status']     	= 'Статус сторінки товару';
+$_['entry_cart_page_status']     		= 'Статус кошика';
+$_['entry_insert_tag']     				= 'Тег для вставки';
+$_['entry_insert_type']     			= 'Тип вставки';
+$_['entry_button_align']     			= 'Вирівнювання кнопки';
+$_['entry_button_size'] 				= 'Розмір кнопки';
+$_['entry_button_color'] 				= 'Колір кнопки';
+$_['entry_button_shape'] 				= 'Форма кнопки';
+$_['entry_button_label'] 				= 'Текст кнопки';
+$_['entry_button_tagline'] 				= 'Підзаголовок кнопки';
+$_['entry_message_status']     			= 'Повідомлення відкладеного платежу';
+$_['entry_message_align']     			= 'Вирівнювання повідомлення';
+$_['entry_message_size'] 				= 'Розмір повідомлення';
+$_['entry_message_layout'] 				= 'Розташування повідомлення';
+$_['entry_message_text_color'] 			= 'Колір тексту повідомлення';
+$_['entry_message_text_size'] 			= 'Розмір тексту повідомлення';
+$_['entry_message_flex_color'] 			= 'Колір банера повідомлення';
+$_['entry_message_flex_ratio'] 			= 'Співвідношення банера повідомлення';
+
+// Help
+$_['help_message_status']				= 'Додає на ваш сайт повідомлення про можливість відкладеного платежу';
+
+// Success
+$_['success_save']						= 'Налаштування модуля PayPal Smart Button збережені';
+
+// Error
+$_['error_permission'] 					= 'У вас немає прав для змінення налаштувань модуля PayPal Smart Button';
